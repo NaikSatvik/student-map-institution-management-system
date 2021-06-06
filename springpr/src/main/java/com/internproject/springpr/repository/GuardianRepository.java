bdf2f2bd4abec58a95abf4bc827f13642fa08daf
Satvik2001Naik <satviknaik19@gnu.ac.in>
2021-06-06 20:48:46 +0530

    Template

--- springpr/src/main/java/com/internproject/springpr/repository/GuardianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.internproject.springpr.repository;
-import com.internproject.springpr.domain.Guardian;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuardianRepository extends JpaRepository<Guardian,Long> {
-    Guardian findByGurEmailAndGurPassAndGurStudentmail(String gurEmail,String gurPass,String gurStudentmail);
-}
