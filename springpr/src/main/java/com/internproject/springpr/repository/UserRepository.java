9fd3cccfb36dc5d627612d66f119346184eb73c6
Satvik2001Naik <satviknaik19@gnu.ac.in>
2021-06-09 00:01:59 +0530

    master

--- springpr/src/main/java/com/internproject/springpr/repository/UserRepository.java
+++ springpr/src/main/java/com/internproject/springpr/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.internproject.springpr.repository;
 import java.util.List;
-
 import com.internproject.springpr.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
